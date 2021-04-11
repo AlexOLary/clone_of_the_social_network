f6a0448f6f306954aab70828db6353709fdaeb3f
AlexOLary <AlexOLary@gmail.com>
2021-04-12 05:05:51 +0700

    social network first steps

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const   Header = () => {
+    return <header className='header'>
+        <img
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSS2wPJwIy1veBJUgzulit-mAOqCk5xBfH3vw&usqp=CAU'/>
+    </header>
+}
+
+export default Header;
