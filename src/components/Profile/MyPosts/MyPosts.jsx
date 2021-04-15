dba38168665c42dcc35a44ddc1c4e1cc09cc65c4
AlexOLary <AlexOLary@gmail.com>
2021-04-16 05:04:00 +0700

    props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
     return <div>
         My posts
         <div>
@@ -11,10 +11,8 @@ const MyPosts = () => {
             <button>Remove</button>
         </div>
         <div className={s.posts}>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post message='Hi, how are React?'/>
+            <Post message="Very well"/>
         </div>
     </div>
 }
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return <div>
         <div className={s.item}>
             <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjJTcYFdKdHZCkOxvGGIEEfyF1y9lVQImp6g&usqp=CAU'/>
-            post 1
+            {props.message}
             <div>
                 <span>Like</span>
             </div>
